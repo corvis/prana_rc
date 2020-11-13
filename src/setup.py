c20ba8c7c2d8099e262aab3c9c18931b2941a515
corvis <corvis.mail@gmail.com>
2020-11-14 00:28:25 +0200

    Working on GH workflow

--- src/setup.py
+++ src/setup.py
@@ -14,21 +14,41 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import codecs
 import os
 from os import path
+
 from setuptools import setup, find_packages
-import prana_rc.__version__
 
 src_dir = path.abspath(path.dirname(__file__))
 root_dir = path.join(src_dir, '..')
 
+# == Read version ==
 version_override = os.environ.get('VERSION_OVERRIDE', None) or None
 
+
+def read_file(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+
+def get_version(rel_path):
+    for line in read_file(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
+
 if not version_override:
-    version = prana_rc.__version__.__version__
+    version = get_version("prana_rc/__version__.py")
 else:
     print("Using overridden version: " + version_override)
     version = version_override
+
+# == END: Read version ==
 
 # Get the long description from the README file
 readme_file = path.join(root_dir, 'README.md')
