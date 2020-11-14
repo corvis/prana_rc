924da88ea3d570a419904e157debd6597426485c
corvis <corvis.mail@gmail.com>
2020-11-15 01:15:50 +0200

    Optimized imports, applied formatting, fixed a few codestyle issues

--- src/prana_rc/contrib/api/dto.py
+++ src/prana_rc/contrib/api/dto.py
@@ -1,23 +1,23 @@
 #    Prana RC
 #    Copyright (C) 2020 Dmitry Berezovsky
-#    
+#
 #    prana is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
 #    the Free Software Foundation, either version 3 of the License, or
 #    (at your option) any later version.
-#    
+#
 #    prana is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #    GNU General Public License for more details.
-#    
+#
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import datetime
-from typing import Optional
 
 import pydantic
+from typing import Optional
 
 from prana_rc.entity import Speed, Mode
 
